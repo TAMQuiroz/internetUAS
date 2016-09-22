399421caab53e06959022d0abebaed5e3d2634a7
Luis Angel Eduardo Soto Flores <LSotoF@grupocogesa.gromero.net>
2016-09-22 15:09:58 -0500

    Grupos de investigacion avance

---
+++ resources/views/investigation/group/index.blade.php
@@ -0,0 +1,53 @@
+@extends('app')
+@section('content')
+    <div class="page-title">
+        <div class="title_left">
+            <h3>Grupos de Investigación</h3>
+        </div>
+        <div class="title_right">
+            <div class="col-md-5 col-sm-5 col-xs-12 form-group pull-right top_search">
+                <div class="input-group">
+
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="clearfix"></div>
+    <div class="col-md-12 col-sm-12 col-xs-12">
+        <div class="x_panel">
+            <div class="x_title">
+                <div class="clearfix"></div>
+                <div class="row">
+                    <div class="col-md-12 col-sm-12 col-xs-12">
+                        @if(in_array(5,Session::get('actions')))
+                            <a href="{{ route('new.courses') }}" class="btn btn-success pull-right">
+                                <i class="fa fa-plus"></i> Nuevo Grupo</a>
+                        @else
+                            <a class="btn btn-success pull-right">
+                                <i class="fa fa-plus"></i> Nuevo Grupo</a>
+                        @endif
+                    </div>
+                </div>
+
+                <div class="x_content">
+                    <div class="clearfix"></div>
+                </div>
+                <table class="table table-striped responsive-utilities jambo_table bulk_action">
+                    <thead>
+                    <tr class="headings">
+                        <th class="column-title">Código </th>
+                        <th class="column-title">Nombre </th>
+                        <th class="column-title last">Acciones</th>
+                        <th class="bulk-actions" colspan="7">
+                            <a class="antoo" style="color:#fff; font-weight:500;">Bulk Actions ( <span class="action-cnt"> </span> ) <i class="fa fa-chevron-down"></i></a>
+                        </th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+
+@endsection

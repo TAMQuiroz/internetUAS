c09df6df5f95a3e44bfa482dfb4c4be6c16d443f
samoelSarmiento <samoel.sarmiento@pucp.pe>
2016-10-20 16:09:17 -0500

    añadi para que el auth y user controller te devuelvan toda la informacion referente al usuario y no solo limitado a acreditador

--- app/Http/Controllers/API/User/UserController.php
+++ app/Http/Controllers/API/User/UserController.php
@@ -3,6 +3,7 @@
 namespace Intranet\Http\Controllers\API\User;
 
 use JWTAuth;
+use Response;
 use Dingo\Api\Routing\Helpers;
 use Illuminate\Routing\Controller as BaseController;
 
@@ -13,8 +14,14 @@ class UserController extends BaseController
     public function getUserInfo()
     {
         $user =  JWTAuth::parseToken()->authenticate();
-        $user->load('professor');
-
-        return $user;
+        $token ="token";
+        if ($user->IdPerfil == 2 || $user->IdPerfil == 1){
+            $user->load('teacher');    
+        }else if ($user->IdPerfil == 4 || $user->IdPerfil == 3){
+            $user->load('accreditor');
+        }else if ($user->IdPerfil == 5){
+            $user->load('investigator');
+        }
+        return Response::json(compact('token', 'user'));
     }
 }

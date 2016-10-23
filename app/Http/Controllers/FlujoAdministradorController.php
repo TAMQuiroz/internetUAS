0de21de3e5e678850fdffafb8aace2948dec1696
Henry Espinoza Torres <henryespinozat@gmail.com>
2016-10-23 15:05:46 -0500

    controller corregido

--- app/Http/Controllers/FlujoAdministradorController.php
+++ app/Http/Controllers/FlujoAdministradorController.php
@@ -93,26 +93,36 @@ class FlujoAdministradorController extends Controller
     }
     
     public function profesor_store (Request $request, $id){
-    	$password = bcrypt(123);
+    	
+        //crear un usuario 
+        $password = bcrypt(123);
 
         $user = User::create([
-            'Usuario' => $request['teachercode'],
+            'Usuario' => $request->input('teachercode'),
             'Contrasena' => $password,
             'IdPerfil' => 2
         ]);
 
+        //crear un nuevo profesor
         $teacher = Teacher::create([
-            'Correo' => $request['teacheremail'],
-            'Nombre' => $request['teachername'],
-            'Codigo' => $request['teachercode'],
-            'ApellidoPaterno' => $request['teacherlastname'],
-            'ApellidoMaterno' => $request['teachersecondlastname'],
+            'Correo' => $request->input('teacheremail'),
+            'Nombre' => $request->input('teachername'),
+            'Codigo' => $request->input('teachercode'),
+            'ApellidoPaterno' => $request->input('teacherlastname'),
+            'ApellidoMaterno' => $request->input('teachersecondlastname'),
             'IdEspecialidad' => $id, //$data['specialty']= Session::get('faculty-code'),
             'IdUsuario' => $user->IdUsuario,
-            'Vigente' => intval($request['teacherstatus']),
-            'Descripcion' => $request['teacherdescription'],
-            'Cargo' => $request['teacherposition']
+            'Vigente' => intval($request->input('teacherstatus')),
+            'Descripcion' => $request->input('teacherdescription'),
+            'Cargo' => $request->input('teacherposition')
         ]);
-    	//return redirect()->route('profesor_index.flujoAdministrador', ['id' => $id]);
+
+        //enviar el corrreo al profesor:
+        if ($user) {
+            $this->passwordService->sendSetPasswordLink($user, $request->input('teacheremail'));
+        }
+
+        return redirect()->route('profesor_index.flujoAdministrador', ['id' => $id])
+                            ->with('success', 'El profesor se ha registrado exitosamente');
     }
 }

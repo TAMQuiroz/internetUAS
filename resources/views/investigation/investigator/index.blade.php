32b1a1115ccb62e413a08df71a6e8e3e68eea40d
Miguel Quiroz <tamquiroz@gmail.com>
2016-10-23 19:05:51 -0500

    arreglo en el .lock

--- resources/views/investigation/investigator/index.blade.php
+++ resources/views/investigation/investigator/index.blade.php
@@ -26,36 +26,38 @@
 		    	<h3 class="panel-title">Investigadores</h3>
 			</div>
 		  	<div class="panel-body">
-				<table class="table table-striped responsive-utilities jambo_table bulk_action"> 
-					<thead> 
-						<tr class="headings"> 
-							<th>Nombre</th> 
-							<th>Apellido Paterno</th> 
-							<th>Apellido Materno</th> 
-							<th>Correo</th> 
-							<th>Especialidad</th> 
-							<th colspan="2">Acciones</th>
-						</tr> 
-					</thead> 
-					<tbody> 
-						@foreach($investigadores as $investigador)
-						<tr> 
-							<td>{{$investigador->nombre}}</td> 
-							<td>{{$investigador->ape_paterno}}</td> 
-							<td>{{$investigador->ape_materno}}</td> 
-							<td>{{$investigador->correo}}</td> 
-							<td>{{$investigador->faculty->Nombre}}</td>
-							<td>
-								<a href="{{route('investigador.show', $investigador->id)}}" class="btn btn-primary btn-xs" title="Visualizar"><i class="fa fa-search"></i></a>
-								<a href="" class="btn btn-danger btn-xs" data-toggle="modal" data-target="#{{$investigador->id}}" title="Eliminar"><i class="fa fa-remove"></i></a>
-							</td>
-						</tr> 
+		  		<div class="table-responsive">
+					<table class="table table-striped responsive-utilities jambo_table bulk_action"> 
+						<thead> 
+							<tr class="headings"> 
+								<th>Nombre</th> 
+								<th>Apellido Paterno</th> 
+								<th>Apellido Materno</th> 
+								<th>Correo</th> 
+								<th>Especialidad</th> 
+								<th colspan="2">Acciones</th>
+							</tr> 
+						</thead> 
+						<tbody> 
+							@foreach($investigadores as $investigador)
+							<tr> 
+								<td>{{$investigador->nombre}}</td> 
+								<td>{{$investigador->ape_paterno}}</td> 
+								<td>{{$investigador->ape_materno}}</td> 
+								<td>{{$investigador->correo}}</td> 
+								<td>{{$investigador->faculty->Nombre}}</td>
+								<td>
+									<a href="{{route('investigador.show', $investigador->id)}}" class="btn btn-primary btn-xs" title="Visualizar"><i class="fa fa-search"></i></a>
+									<a href="" class="btn btn-danger btn-xs" data-toggle="modal" data-target="#{{$investigador->id}}" title="Eliminar"><i class="fa fa-remove"></i></a>
+								</td>
+							</tr> 
 
-						@include('modals.delete', ['id'=> $investigador->id, 'message' => '¿Esta seguro que desea eliminar este investigador?', 'route' => route('investigador.delete', $investigador->id)])
-						@endforeach
-						
-					</tbody> 
-				</table>
+							@include('modals.delete', ['id'=> $investigador->id, 'message' => '¿Esta seguro que desea eliminar este investigador?', 'route' => route('investigador.delete', $investigador->id)])
+							@endforeach
+							
+						</tbody> 
+					</table>
+				</div>
 		  	</div>
 		</div>
     </div>
---
+++ tests/DeliverableTest.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class DeliverableTest extends TestCase
+{
+	use DatabaseMigrations;
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function test_inv_del_cr_01()
+    {
+        $this->assertTrue(true);	
+    }
+}

e4953a103b95263272e9572c63c363bee49969d5
Caluuvi <claudia.villanuevac@pucp.pe>
2016-10-23 22:47:16 -0500

    arreglando merge problem, vista de proceso en progreso

--- resources/views/psp/pspProcess/create.blade.php
+++ resources/views/psp/pspProcess/create.blade.php
@@ -4,7 +4,7 @@
     <div class="col-md-12">
         <div class="page-title">
             <div class="title_left">
-                <h3>Creación de Supervisor</h3>
+                <h3>Activar nuevo ciclo PSP</h3>
             </div>
         </div>
     </div>
@@ -20,15 +20,15 @@
             {{Form::open(['route' => 'pspProcess.store','class'=>'form-horizontal', 'id'=>'formSuggestion'])}}
 
                 <div class="form-group">
-                    {{Form::label('Curso *',null,['class'=>'control-label col-md-4 col-sm-3 col-xs-12'])}}
+                    {{Form::label('Seleccionar curso de PSP *',null,['class'=>'control-label col-md-4 col-sm-3 col-xs-12'])}}
                     <div class="col-md-4">
-                        {{ Form::select('codigo', [1, 2, 3], null) }}
+                        {{ Form::select('IdCurso', $courses, null, ['class'=>'control-label']) }}
                     </div>
                 </div>   
 
                 <div class="row">
                     <div class="col-md-8 col-sm-12 col-xs-12">
-                        {{Form::submit('Guardar', ['class'=>'btn btn-success pull-right'])}}
+                        {{Form::submit('Activar', ['class'=>'btn btn-success pull-right'])}}
                         <a class="btn btn-default pull-right" href="{{ route('pspProcess.index') }}">Cancelar</a>
                     </div>
                 </div>

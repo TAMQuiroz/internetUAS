f053fa527f98dbf4b675f48cc046324a59a47eb1
Jonatan Gonzales <jonatan.gonzales@pucp.pe>
2016-10-27 03:35:35 -0500

    vista del tutor

--- app/Http/Controllers/Tutorship/MyTutor/MyTutorController.php
+++ app/Http/Controllers/Tutorship/MyTutor/MyTutorController.php
@@ -22,44 +22,10 @@ class MyTutorController extends Controller {
         }
 
         $horarios = TutSchedule::where('id_docente', $tutor->IdDocente)->orderBy('dia', 'asc')->orderBy('hora_inicio', 'asc')->get();
-
-        $lun = [];
-        $mar = [];
-        $mie = [];
-        $jue = [];
-        $vie = [];
-        $sab = [];
-
-        foreach ($horarios as $horario) {
-            if ($horario->dia == 1) {
-                array_push($lun, $horario->hora_inicio);
-            }
-            if ($horario->dia == 2) {
-                array_push($mar, $horario->hora_inicio);
-            }
-            if ($horario->dia == 3) {
-                array_push($mie, $horario->hora_inicio);
-            }
-            if ($horario->dia == 4) {
-                array_push($jue, $horario->hora_inicio);
-            }
-            if ($horario->dia == 5) {
-                array_push($vie, $horario->hora_inicio);
-            }
-            if ($horario->dia == 6) {
-                array_push($sab, $horario->hora_inicio);
-            }
-        }
-
+        
         $data = [
             'tutor' => $tutor,
-            'horarios' => $horarios,
-            'lun' => $lun,
-            'mar' => $mar,
-            'mie' => $mie,
-            'jue' => $jue,
-            'vie' => $vie,
-            'sab' => $sab,
+            'horarios' => $horarios,            
         ];
         return view('tutorship.mytutor.index', $data);
     }

a0fda633efbd2d6753a1d0b32045c034d947a14c
valeria <v.urquizo@pucp.pe>
2016-10-28 11:58:35 -0500

    pruebas - flujo administrador

--- tests/FlujoAdministradorTest.php
+++ tests/FlujoAdministradorTest.php
@@ -36,45 +36,12 @@ class FlujoAdministradorTest extends TestCase
     		->seePageIs('/flujoAdministrador/academicCycle/')
     		->see('El ciclo académico se ha registrado exitosamente');
     }
-
-    public function test_uas_crear_cicloacademico_02(){
-
-        $user = factory(Intranet\Models\User::class)->make();
-
-        $this->actingAs($user)
-            ->withSession([
-                'actions' => [],
-                'user' => $user
-            ])->visit('/flujoAdministrador/academicCycle/create')
-            ->select('0','anio')
-            ->select('1','numberC')
-            ->press('Guardar')
-            ->seePageIs('/flujoAdministrador/academicCycle/create/')
-            ->see('Debe seleccionar el año del ciclo');
-
-    }
-
-    public function test_uas_crear_cicloacademico_03(){
-
-        $user = factory(Intranet\Models\User::class)->make();
-
-        $this->actingAs($user)
-            ->withSession([
-                'actions' => [],
-                'user' => $user
-            ])->visit('/flujoAdministrador/academicCycle/create')
-            ->select('2020','anio')
-            ->select('3','numberC')
-            ->press('Guardar')
-            ->seePageIs('/flujoAdministrador/academicCycle/create/')
-            ->see('Debe seleccionar el número del ciclo');
-    }
-
+    
     public function test_uas_crear_profesor_01(){
 
         $user = factory(Intranet\Models\User::class)->make();
         //$facultad = factory(Intranet\Models\Faculty::class)->make();
-        
+
         $this->actingAs($user)
             ->withSession([
                 'actions' => [],

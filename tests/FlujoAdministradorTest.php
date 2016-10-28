80839ae2efd947a0f0d04ce4ff96eea4750b3c62
valeria <v.urquizo@pucp.pe>
2016-10-28 11:19:23 -0500

    mas pruebas flujo administrador - ciclo academico

--- tests/FlujoAdministradorTest.php
+++ tests/FlujoAdministradorTest.php
@@ -29,12 +29,43 @@ class FlujoAdministradorTest extends TestCase
 	    		'actions' => [],
 	    		'user' => $user
     		])->visit('/flujoAdministrador/academicCycle/create')
-    		->select('2016','anio')
+    		->select('2020','anio')
     		->select('1','numberC')
     		->press('Guardar')
     		->seePageIs('/flujoAdministrador/academicCycle/')
     		->see('El ciclo académico se ha registrado exitosamente');
     }
 
+    public function test_uas_crear_cicloacademico_02(){
+
+        $user = factory(Intranet\Models\User::class)->make();
+
+        $this->actingAs($user)
+            ->withSession([
+                'actions' => [],
+                'user' => $user
+            ])->visit('/flujoAdministrador/academicCycle/create')
+            ->select('0','anio')
+            ->select('1','numberC')
+            ->press('Guardar')
+            ->seePageIs('/flujoAdministrador/academicCycle/create/')
+            ->see('Debe seleccionar el año del ciclo');
+    }
+
+    public function test_uas_crear_cicloacademico_03(){
+
+        $user = factory(Intranet\Models\User::class)->make();
+
+        $this->actingAs($user)
+            ->withSession([
+                'actions' => [],
+                'user' => $user
+            ])->visit('/flujoAdministrador/academicCycle/create')
+            ->select('2020','anio')
+            ->select('3','numberC')
+            ->press('Guardar')
+            ->seePageIs('/flujoAdministrador/academicCycle/create/')
+            ->see('Debe seleccionar el número del ciclo');
+    }
 
 }

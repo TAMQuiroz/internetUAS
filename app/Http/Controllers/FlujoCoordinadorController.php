b4edb9790f0ef1f01e265fece470f14885a3448e
angelgsandoval <angelgsandoval@outlook.com>
2016-10-30 16:51:24 -0500

    Agregada pantalla de Objetivos Educacionales

--- app/Http/Controllers/FlujoCoordinadorController.php
+++ app/Http/Controllers/FlujoCoordinadorController.php
@@ -9,6 +9,8 @@ use Intranet\Models\criterion;
 
 use Session;
 use Intranet\Models\Faculty;
+use Intranet\Models\EducationalObjetive;
+use Intranet\Http\Requests\EducationalObjetiveRequest;
 
 class FlujoCoordinadorController extends Controller
 {
@@ -34,48 +36,30 @@ class FlujoCoordinadorController extends Controller
 
 		$especialidad = Faculty::findOrFail($id);
 		$objetivos = $especialidad->objectives;
-		return view('flujoCoordinador.objetivoEducacional_index', ['teachers'=>$objetivos, 'idEspecialidad' =>$id]);
-    	
+		return view('flujoCoordinador.objetivoEducacional_index', ['objetivos'=>$objetivos, 'idEspecialidad' =>$id]);
+
     }
 
-
-    public function profesor_create ($id){
+    public function objetivoEducacional_create ($id){
     	//return 'crear objetivo de la especialidad '.$id;
-    	return view('flujoAdministrador.profesor_create', ['idEspecialidad'=>$id]);
+    	return view('flujoCoordinador.objetivoEducacional_create', ['idEspecialidad'=>$id]);
     }
     
-    public function profesor_store (Request $request, $id){
-    	
-        //crear un usuario 
-        $password = bcrypt(123);
+    public function objetivoEducacional_store (EducationalObjetiveRequest $request, $id){
 
-        $user = User::create([
-            'Usuario' => $request->input('teachercode'),
-            'Contrasena' => $password,
-            'IdPerfil' => 2
-        ]);
+        //crear un nuevo objetivo educacional
+        $numberOE = EducationalObjetive::where('IdEspecialidad',Session::get('faculty-code'))
+									   ->where('deleted_at',null)->count();
+		$numberOE = ($numberOE) + 1;
+		$educationalObjetive = EducationalObjetive::create([
+			'IdEspecialidad' => $id,
+			'Numero' => $numberOE,
+			'Descripcion' => $request->input('descripcion'),
+			'Estado' => 1,
+		]);
 
-        //crear un nuevo profesor
-        $teacher = Teacher::create([
-            'Correo' => $request->input('teacheremail'),
-            'Nombre' => $request->input('teachername'),
-            'Codigo' => $request->input('teachercode'),
-            'ApellidoPaterno' => $request->input('teacherlastname'),
-            'ApellidoMaterno' => $request->input('teachersecondlastname'),
-            'IdEspecialidad' => $id, //$data['specialty']= Session::get('faculty-code'),
-            'IdUsuario' => $user->IdUsuario,
-            'Vigente' => intval($request->input('teacherstatus')),
-            'Descripcion' => $request->input('teacherdescription'),
-            'Cargo' => $request->input('teacherposition')
-        ]);
-
-        //enviar el corrreo al profesor:
-        if ($user) {
-            $this->passwordService->sendSetPasswordLink($user, $request->input('teacheremail'));
-        }
-
-        return redirect()->route('profesor_index.flujoAdministrador', ['id' => $id])
-                            ->with('success', 'El profesor se ha registrado exitosamente');
+        return redirect()->route('objetivoEducacional_index.flujoCoordinador', ['id' => $id])
+                            ->with('success', 'El objetivo educacional se ha registrado exitosamente');
     }
 
 }
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1086,8 +1086,6 @@ Route::group(['prefix' => 'tutoria'], function(){
             Route::get('delete/{id}', ['as' => 'evaluacion.delete', 'uses' => 'Evaluations\Evaluation\EvaluationController@destroy']);
             Route::get('cancel/{id}', ['as' => 'evaluacion.cancel', 'uses' => 'Evaluations\Evaluation\EvaluationController@cancel']);
             Route::get('activate/{id}', ['as' => 'evaluacion.activate', 'uses' => 'Evaluations\Evaluation\EvaluationController@activate']);
-
-            
         });
     });
 
@@ -1098,9 +1096,9 @@ Route::group(['prefix' => 'tutoria'], function(){
         Route::get('/{id}/objetivoEducacional/create', ['as' => 'objetivoEducacional_create.flujoCoordinador', 'uses' => 'FlujoCoordinadorController@objetivoEducacional_create']);        
         Route::post('/{id}/objetivoEducacional/store', ['as' => 'objetivoEducacional_store.flujoCoordinador', 'uses' => 'FlujoCoordinadorController@objetivoEducacional_store']);
         
-        Route::get('/{id}/aspecto', ['as' => 'objetivoEducacional_index.flujoCoordinador', 'uses' => 'FlujoCoordinadorController@objetivoEducacional_index']);
-        Route::get('/{id}/aspecto/create', ['as' => 'objetivoEducacional_create.flujoCoordinador', 'uses' => 'FlujoCoordinadorController@objetivoEducacional_create']);        
-        Route::post('/{id}/aspecto/store', ['as' => 'objetivoEducacional_store.flujoCoordinador', 'uses' => 'FlujoCoordinadorController@objetivoEducacional_store']);
+        Route::get('/{id}/aspecto', ['as' => 'aspecto_index.flujoCoordinador', 'uses' => 'FlujoCoordinadorController@aspecto_index']);
+        Route::get('/{id}/aspecto/create', ['as' => 'aspecto_create.flujoCoordinador', 'uses' => 'FlujoCoordinadorController@aspecto_create']);        
+        Route::post('/{id}/aspecto/store', ['as' => 'aspecto_store.flujoCoordinador', 'uses' => 'FlujoCoordinadorController@aspecto_store']);
 
         Route::get('/{id}/criterio', ['as' => 'criterio_index.flujoCoordinador', 'uses' => 'FlujoCoordinadorController@criterio_index']);
         Route::get('/{id}/criterio/create', ['as' => 'criterio_create.flujoCoordinador', 'uses' => 'FlujoCoordinadorController@criterio_create']);        

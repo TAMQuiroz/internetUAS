c678d1a8bd1591b97fae17df1be2e4e10230b9f5
alex <alexander.merino@pucp.pe>
2016-10-30 19:34:02 -0500

    arreglar ventana de aspectos

--- resources/views/flujoCoordinador/aspect_index.blade.php
+++ resources/views/flujoCoordinador/aspect_index.blade.php
@@ -12,11 +12,7 @@
     <div class="x_content">
       <form action="{{ route('aspect_create.flujoCoordinador',$idEspecialidad)}}" method="POST" id="formAspect" name="formAspect" novalidate="true" class="form-horizontal">
         <input type="hidden" name="_token" value="{{ csrf_token() }}">
-        <div class="row">
-            <div class="col-md-9 col-sm-12 col-xs-12">
-               <button class="btn btn-success pull-right" type="submit"> Nuevo Aspecto</button>
-            </div>
-        </div>
+        
         <div class="form-horizontal">
             <div class="row" style="margin-top: 10px;margin-bottom: 10px;">
               <label for="middle-name" class="control-label col-md-3 col-sm-3 col-xs-12">Resultado Estudiantil (RE)</label>
@@ -32,6 +28,11 @@
                   
                 </select>
               </div>
+            </div>
+            <div class="row">
+              
+                 <button class="btn btn-success pull-right" type="submit"> Nuevo Aspecto</button>
+              
             </div>
         </div>
       </form>
@@ -49,9 +50,9 @@
       <table class="table table-striped responsive-utilities jambo_table bulk_action" id="aspect-table">
         <thead>
           <tr class="headings">
-            <th class="col-sm-2 column-title">Estado</th>
-            <th class="col-sm-1  column-title">RE</th>
+            <th class="col-sm-3  column-title">Resultado Estudiantil</th>
             <th class="col-sm-7 column-title">Nombre del Aspecto</th>
+            <th class="col-sm-2 column-title">Estado</th>
           </tr>
         </thead>
 
@@ -62,13 +63,15 @@
               <td class="aspect_code" hidden="true">{{ $asp->IdAspecto}}</td>
               <td class="aspect_studentsResult" hidden="true">{{ $asp->studentsResult->IdResultadoEstudiantil}}</td>
               <td class="aspect_studentsResultName" hidden="true">{{$asp->studentsResult->Identificador}} - {{$asp->studentsResult->Descripcion}}</td>
+              
+              <td class="aspect_re">{{$asp->studentsResult->Identificador}}</td>
+              <td class="aspect_name">{{ str_limit($asp->Nombre,40)}}</td>
               @if($asp->Estado == 1)
               <td class=""><span class="label label-success"> Activo </span></td>
               @else
               <td class=""><span class="label label-danger"> Inactivo </span></td>
               @endif
-              <td class="aspect_re">{{$asp->studentsResult->Identificador}}</td>
-              <td class="aspect_name">{{ str_limit($asp->Nombre,40)}}</td>
+
             </tr>
           @endforeach
 

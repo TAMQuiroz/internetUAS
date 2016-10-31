43aeb3b7f51aea93e393f6126fa893f7f9631abf
Marshall Fernandez <marshall.fernandez@pucp.pe>
2016-10-30 19:48:47 -0500

    api period controller fix

--- app/Http/Controllers/API/Period/PeriodController.php
+++ app/Http/Controllers/API/Period/PeriodController.php
@@ -104,7 +104,17 @@ class PeriodController extends BaseController
 
     public function getPeriodbyId($period_id){
       $period = Period::where('IdPeriodo',$period_id)->with('configuration')->first();
-      return Response::json($period);
+
+      if(is_null($period)) return response()->json($period);
+
+      $init_semester = $period->configuration->cycleAcademicStart->Numero;
+        $end_semester = $period->configuration->cycleAcademicEnd->Numero;
+        $period->semesters = AcademicCycle::where('Numero', '>=', $init_semester)
+                                          ->where('Numero', '<=', $end_semester)
+                                          ->orderBy('Numero', 'asc')
+                                          ->get();
+      
+      return response()->json($period);
     }
 
 

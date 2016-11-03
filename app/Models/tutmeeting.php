9b356bbafe50eaf16f0123be81eb5714e4c7a0b8
Miguel Quiroz <tamquiroz@gmail.com>
2016-11-03 09:29:48 -0500

    merge fix

--- app/Models/tutmeeting.php
+++ app/Models/tutmeeting.php
@@ -2,9 +2,26 @@
 
 namespace Intranet\Models;
 
+use DB;
 use Illuminate\Database\Eloquent\Model;
 
-class tutmeeting extends Model
-{
-    //
+class TutMeeting extends Model
+{    
+    protected $table = 'tutmeetings';
+    protected $primaryKey = 'id';
+    protected $fillable = ['inicio',
+                            'fin',
+                            'duracion',
+                            'no_programada',
+                            'observacion',
+                            'lugar',
+                            'adicional',
+                            'creador',
+                            'estado',
+                            'id_topic',
+                            'id_reason',
+                            'id_tutstudent0',
+                            'id_docente'];
+    
+    
 }

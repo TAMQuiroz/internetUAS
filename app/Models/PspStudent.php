8a74c62e718632c28e531dbc9556e967036c5d32
Caluuvi <claudia.villanuevac@pucp.pe>
2016-11-07 00:08:28 -0500

    creacion de pspstudent + accesos a modulo

--- app/Models/PspStudent.php
+++ app/Models/PspStudent.php
@@ -3,10 +3,15 @@
 namespace Intranet\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Intranet\Models\Traits\LastUpdatedTrait;
 
 class PspStudent extends Model
 {
     //
+    use SoftDeletes;
+    use LastUpdatedTrait;
+
     protected $table = 'pspstudents';
 
     public function student(){

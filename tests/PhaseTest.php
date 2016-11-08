1d495a156c662fc4ba2aa55f9a3a1dd79dcfe854
ramon bruderer vega <ramon bruderer vega>
2016-11-08 15:42:49 -0500

    avance pruebas fases

--- tests/PhaseTest.php
+++ tests/PhaseTest.php
@@ -197,6 +197,137 @@ class PhaseTest extends TestCase
             ->see('Fase');
     }
 
+    public function test_psp_ed_pha_02()
+    {
+        $phase = factory(Intranet\Models\Phase::class)->create();
+        $user = factory(Intranet\Models\User::class)->make();
+        $user->IdPerfil=3;
+        $psp = factory(Intranet\Models\PspProcess::class)->create(); 
+
+        $this->actingAs($user)
+            ->withSession([
+                'actions' => [],
+                'user' => $user
+            ])->visit('/psp/phase/edit/'.$phase->id)
+            ->type(13,'numero')
+            ->type('sdsdsfsdfs','descripcion')
+            ->type('1992-08-20','fecha_inicio')
+            ->type('1992-09-20','fecha_fin')
+            ->select($psp->id, 'Proceso_de_Psp') 
+            ->press('Guardar')
+            ->seePageIs('/psp/phase/edit/'.$phase->id)
+            ->see('Fase');
+    }
+
+    public function test_psp_ed_pha_03()
+    {
+        $phase = factory(Intranet\Models\Phase::class)->create();
+        $user = factory(Intranet\Models\User::class)->make();
+        $user->IdPerfil=3;
+        $psp = factory(Intranet\Models\PspProcess::class)->create(); 
+
+        $this->actingAs($user)
+            ->withSession([
+                'actions' => [],
+                'user' => $user
+            ])->visit('/psp/phase/edit/'.$phase->id)
+            ->type(NULL,'numero')
+            ->type('djd','descripcion')
+            ->type('1992-08-20','fecha_inicio')
+            ->type('1992-09-20','fecha_fin')
+            ->select($psp->id, 'Proceso_de_Psp') 
+            ->press('Guardar')
+            ->seePageIs('/psp/phase/edit/'.$phase->id)
+            ->see('Fase');
+    }
+
+    public function test_psp_ed_pha_04()
+    {
+        $phase = factory(Intranet\Models\Phase::class)->create();
+        $user = factory(Intranet\Models\User::class)->make();
+        $user->IdPerfil=3;
+        $psp = factory(Intranet\Models\PspProcess::class)->create(); 
+
+        $this->actingAs($user)
+            ->withSession([
+                'actions' => [],
+                'user' => $user
+            ])->visit('/psp/phase/edit/'.$phase->id)
+            ->type(-5,'numero')
+            ->type('','descripcion')
+            ->type('1992-08-20','fecha_inicio')
+            ->type('1992-09-20','fecha_fin')
+            ->select($psp->id, 'Proceso_de_Psp')
+            ->press('Guardar')
+            ->seePageIs('/psp/phase/edit/'.$phase->id)
+            ->see('Fase');
+    }
+
+    public function test_psp_ed_pha_05()
+    {
+        $phase = factory(Intranet\Models\Phase::class)->create();
+        $user = factory(Intranet\Models\User::class)->make();
+        $user->IdPerfil=3;
+        $psp = factory(Intranet\Models\PspProcess::class)->create(); 
+
+        $this->actingAs($user)
+            ->withSession([
+                'actions' => [],
+                'user' => $user
+            ])->visit('/psp/phase/edit/'.$phase->id)
+            ->type(5,'numero')
+            ->type('','descripcion')
+            ->type('1992-08-20','fecha_inicio')
+            ->type('1992-09-20','fecha_fin')
+            ->select($psp->id, 'Proceso_de_Psp') 
+            ->press('Guardar')
+            ->seePageIs('/psp/phase/edit/'.$phase->id)
+            ->see('Fase');
+    }
+
+    public function test_psp_ed_pha_06()
+    {
+        $phase = factory(Intranet\Models\Phase::class)->create();
+        $user = factory(Intranet\Models\User::class)->make();
+        $user->IdPerfil=3;
+        $psp = factory(Intranet\Models\PspProcess::class)->create(); 
+
+        $this->actingAs($user)
+            ->withSession([
+                'actions' => [],
+                'user' => $user
+            ])->visit('/psp/phase/edit/'.$phase->id)
+            ->type(4,'numero')
+            ->type('DSDDDDDDsdsds','descripcion')
+            ->type(NULL,'fecha_inicio')
+            ->type('1992-09-20','fecha_fin')
+            ->select($psp->id, 'Proceso_de_Psp') 
+            ->press('Guardar')
+            ->seePageIs('/psp/phase/edit/'.$phase->id)
+            ->see('Fase');
+    }
+
+    public function test_psp_ed_pha_07()
+    {
+        $phase = factory(Intranet\Models\Phase::class)->create();
+        $user = factory(Intranet\Models\User::class)->make();
+        $user->IdPerfil=3;
+        $psp = factory(Intranet\Models\PspProcess::class)->create(); 
+
+        $this->actingAs($user)
+            ->withSession([
+                'actions' => [],
+                'user' => $user
+            ])->visit('/psp/phase/edit/'.$phase->id)
+            ->type(4,'numero')
+            ->type('DSDDDDDDsdsds','descripcion')
+            ->type('1992-09-20','fecha_inicio')
+            ->type(NULL,'fecha_fin')
+            ->select($psp->id, 'Proceso_de_Psp') 
+            ->press('Guardar')
+            ->seePageIs('/psp/phase/edit/'.$phase->id)
+            ->see('Fase');
+    }   
     
 }
 

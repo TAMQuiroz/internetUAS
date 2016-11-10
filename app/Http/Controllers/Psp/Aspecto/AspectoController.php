abe770c9f614ebcff26f22440c770722873207aa
willypf <willypf21@hotmail.com>
2016-11-09 22:05:09 -0500

    vista de asignacion de notas x criterio a los alumnos.

--- app/Http/Controllers/Psp/Aspecto/AspectoController.php
+++ app/Http/Controllers/Psp/Aspecto/AspectoController.php
@@ -2,12 +2,18 @@
 
 namespace Intranet\Http\Controllers\Psp\Aspecto;
 
+use Illuminate\Support\Facades\Session;
 use Illuminate\Http\Request;
 use Intranet\Models\Student;
 use Intranet\Models\Aspect; /*ya existia en el uas */
 use Intranet\Models\User;
+use Intranet\Models\Pspstudentsxcriterios;
 use Intranet\Models\Supervisor;
 use Intranet\Models\PspDocument;
+use Intranet\Models\Criterion;
+use Intranet\Models\CoursexCycle;
+use Intranet\Models\PspProcess;
+use Intranet\Models\CoursexCyclexCriterion;
 use Intranet\Http\Requests;
 use Intranet\Http\Controllers\Controller;
 use Intranet\Models\PspStudent;
@@ -37,9 +43,42 @@ class AspectoController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function create()
+    public function create($idAlumno)
     {
         //
+        //
+
+        $user = Session::get('user');
+
+        $supervisor = Supervisor::find($user->id);
+        //echo "userid ".$user->id;
+        //echo " supervisorid ".$supervisor->idpspprocess;
+
+        $pspProceso = PspProcess::find($supervisor->idpspprocess);
+        
+        $cursoxciclo = CoursexCycle::where('IdCurso',$pspProceso->idcurso)->first();
+        //echo $pspProceso->idcurso;
+        //echo $pspProceso->idcurso;
+        $criterios = CoursexCyclexCriterion::where('IdCursoxCiclo', $cursoxciclo->IdCursoxCiclo)->get();
+        //echo "idcursoxciclo".$cursoxciclo->IdCursoxCiclo;
+
+        $registroNotas = Pspstudentsxcriterios::where('idpspstudent',$idAlumno)->get();
+        
+        //$crit_aux       = Criterion::find($id);
+        $crit = [];
+        foreach ($criterios as $c) {
+            $criterio = Criterion::find($c->IdCriterio);
+            $crit[$c->IdCriterio]=$criterio->Nombre;
+            echo $criterio->Nombre;
+        }
+
+        $data = [
+            'crit'    =>  $crit,
+            'idAlumno' => $idAlumno,
+            'registroNotas' => $registroNotas,
+            //'crit_aux' => $crit_aux,
+        ];
+        return view('psp.aspecto.create', $data);
     }
 
     /**
@@ -48,10 +87,39 @@ class AspectoController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(Request $request, $idAlumno)
     {
-        //
-        //$nota = $request['nota'];  //con esto se obtiene lo que se selecciona con el combobox
+        $listaNotas = $request->input("nota",[]);
+        foreach ($listaNotas as $idCrit => $valorNota) {
+            //echo "$idCrit"." ".$valorNota."<br>";
+        }
+        try {
+               $listaNotas = $request->input("nota",[]);
+                foreach ($listaNotas as $idCrit => $valorNota) {
+
+                    $p = Pspstudentsxcriterios::where('idpspstudent',$idAlumno)->where('IdCriterio',$idCrit)->first();
+                    if ($p->count()==0){
+                        $pspstudentxcriterios  = new Pspstudentsxcriterios;
+                        $pspstudentxcriterios->idpspstudent = $idAlumno;
+                        $pspstudentxcriterios->idcriterio = $idCrit;
+                        $pspstudentxcriterios->nota = $valorNota;
+                        $pspstudentxcriterios->save();
+                    } else {
+                        $pN = Pspstudentsxcriterios::find($p->id);
+                        $pN->nota = $valorNota;
+                        $pN->save();
+                    }
+
+                    $pspstudentxcriterios  = new Pspstudentsxcriterios;
+                    $pspstudentxcriterios->idpspstudent = $idAlumno;
+                    $pspstudentxcriterios->idcriterio = $idCrit;
+                    $pspstudentxcriterios->nota = $valorNota;
+                    $pspstudentxcriterios->save();
+                }
+                return redirect()->route('student.index')->with('success', 'La información se ha registrado exitosamente');                
+            }catch (Exception $e){
+                 return redirect()->back()->with('warning', 'Ocurrió un error al hacer esta acción');
+        }         
     }
 
     /**
@@ -74,11 +142,17 @@ class AspectoController extends Controller
     public function edit($id)
     {
         //
-
-        $aspectos = null;
+        $criterios = CoursexCyclexCriterion::where('IdCursoxCiclo', 22)->get();
+        //$crit_aux       = Criterion::find($id);
+        $crit = [];
+        foreach ($criterios as $c) {
+            $criterio = Criterion::find($c->IdCriterio);
+            $crit[$c->IdCriterio]=$criterio->Nombre;
+        }
 
         $data = [
-            'aspectos'    =>  $aspectos,
+            'crit'    =>  $crit,
+            //'crit_aux' => $crit_aux,
         ];
         return view('psp.aspecto.edit', $data);
     }
@@ -90,10 +164,22 @@ class AspectoController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(CriterioRequest $request, $id)
     {
-        //
+        try {
+            //Crear 
+            $criterios                   = Criterio::find($id);
+            $criterios->Nombre          = $request['Nombre'];
+            $criterios->Estado          = $request['Estado'];            
+            
+            $criterios->save();
+
+            return redirect()->route('student.index')->with('success', 'Las notas por criterio asignadas al alumno se han actualizado exitosamente');
+        } catch (Exception $e){
+            return redirect()->back()->with('warning', 'Ocurrió un error al hacer esta acción');
+        }
     }
+    
 
     /**
      * Remove the specified resource from storage.

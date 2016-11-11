479ad26b2383228cdd83f7f5b0850ebf5e38667d
caluuvi <claudia.villanuevac@pucp.pe>
2016-11-10 23:50:39 -0500

    seeder para supervisor y alumno psp

--- database/seeds/PspProcessTableSeeder.php
+++ database/seeds/PspProcessTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Faker\Factory as Faker; 
 
 class PspProcessTableSeeder extends Seeder
 {
@@ -13,6 +14,7 @@ class PspProcessTableSeeder extends Seeder
     {
         //
         DB::table('pspprocesses')->insert([
+            'id'        =>  1,
             'numero_Fases' => 0,
             'numero_Plantillas' => 0,
             'max_tam_plantilla' => 0,
@@ -26,8 +28,93 @@ class PspProcessTableSeeder extends Seeder
             'iddocente' => 4,
         ]);        
 
-        //crear alumno
-
         //crear supervisor
+        $faker = Faker::create();
+        $usuario = DB::table('Usuario')->where('IdUsuario',40)->first();
+
+        if(!$usuario){
+            DB::table('Usuario')->insert([
+                'IdUsuario' => 40,
+                'IdPerfil'          => 6,
+                'Usuario'           => 20112188,
+                'Contrasena'        => bcrypt(20112188),
+            ]);
+        }
+
+        $supervisor = DB::table('supervisors')->where('id',40)->first();
+        if(!$supervisor){
+            DB::table('supervisors')->insert([
+                'id'                =>  40,
+                'nombres'            =>  $faker->firstNameMale,
+                'apellido_paterno'   =>  $faker->lastName,
+                'apellido_materno'   =>  $faker->lastName,
+                'correo'            =>  $faker->email,
+                'direccion'         =>  'av 123',
+                'telefono'          => 123412341,
+                'codigo_trabajador' => 20112188,
+                'idfaculty'         =>  1,
+                'iduser'            =>  40,
+                'idpspprocess'      =>  1,
+                'vigente'           =>  1,
+            ]);
+
+            DB::table('pspprocessesxsupervisors')->insert([   
+                'idpspprocess' => 1,
+                'idsupervisor' => 40,
+            ]); 
+        }
+
+        //crear alumno
+        $userAlumno = DB::table('Usuario')->where('IdUsuario',41)->first();
+
+        if(!$userAlumno){
+            DB::table('Usuario')->insert([
+                'IdUsuario'         => 41,
+                'IdPerfil'          => 0,
+                'Usuario'           => 45674567,
+                'Contrasena'        => bcrypt(45674567),
+            ]);
+        }
+
+        $alumno = DB::table('tutstudents')->where('id',41)->first();
+
+        if(!$alumno){
+            DB::table('tutstudents')->insert([
+                'id'         => 41,
+                'id_usuario'     =>  41,
+                'codigo'           => 45674567,
+                'nombre'            =>  'Juan',
+                'ape_paterno'   =>  'Perez',
+                'ape_materno'   =>  'Perez',
+                'correo'            =>  'asdsad@dajsd.com',
+                'id_especialidad'   =>  1,
+            ]);
+        }
+
+        $alumnoUAS = DB::table('alumno')->where('IdAlumno',41)->first();
+
+        if(!$alumnoUAS){
+            DB::table('alumno')->insert([
+                'IdAlumno'         => 41,
+                'Codigo'           => 45674567,
+                'Nombre'            =>  'Juan',
+                'ApellidoPaterno'   =>  'Perez',
+                'ApellidoMaterno'   =>  'Perez',
+                'IdUsuario'   =>  41,
+                'IdHorario'     =>  6,
+                'lleva_psp'     =>  1,
+            ]);
+        }
+
+        $alumnoPSP = DB::table('pspstudents')->where('id',41)->first();
+
+        if(!$alumnoPSP){
+            DB::table('pspstudents')->insert([
+                'id'         => 41,
+                'idalumno'      =>  41,
+                'idespecialidad'    =>  1,
+                'idpspprocess'  =>  1,
+            ]);
+        }
     }
 }

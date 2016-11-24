39c32c2ce11bc763292f7da23150e67babf87d09
Sephirihos <sephirihos18@gmail.com>
2016-11-24 00:49:22 -0500

    fixing psp movil

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -845,7 +845,10 @@ $api->version('v1', function ($api) {
                  $api->get('student',"Students\PspStudentsController@getStudent");
                  $api->get('supervisor/freehours',"FreeHour\PspFreeHourController@showFreeHourForSupervisor");  
 
-                 $api->get('students/finalScore',"Students\PspStudentsController@getStudentsFinalScore");    
+                 $api->get('students/finalScore',"Students\PspStudentsController@getStudentsFinalScore");   
+
+                 $api->get('meeting/status',"Meeting\PspMeetingController@getMeetingStatus"); 
+
 
 
 

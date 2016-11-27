2115508e3497b03d7cbe6fbbd2054ac7c5c0674c
Luis Llanos <llanosl@bitmap.pe>
2016-11-27 18:09:50 -0500

    blind code: to be tested

--- resources/views/psp/freeHour/create.blade.php
+++ resources/views/psp/freeHour/create.blade.php
@@ -25,56 +25,72 @@
                     <input class="form-control" type="text" name="fecha" id="fecha" placeholder="dd-mm-aaaa" minlength="10" maxlength="10" value="" required/>
                 </div>
             </div>
-                
-                <div class="form-group">
-                    {{Form::label('Disponibilidad *',null,['class'=>'control-label col-md-4 col-sm-3 col-xs-12'])}}
-                    <div class="col-md-4">
-                        <table class="table table-striped responsive-utilities jambo_table bulk_action">
-                            <thead>
-                                <tr class="headings">
-                                    <th class="centered column-title">Hora</th>
-                                    <th class="centered column-title"></th>                                        
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @for ($h=8; $h<22; $h++)
-                                <tr class="even pointer">                                                                     
-                                    <td class="centered ">{{$h}}:00 hs</td>
-                                    <td class="centered "> {{Form::radio('hora_ini',$h , false, array('class' => 'hora_ini'))}} </td>                                                    
-                                </tr>
-                                @endfor
-                            </tbody>
-                        </table>
-                    </div>
+
+            <div class="form-group">
+                {{Form::label('Disponibilidad *',null,['class'=>'control-label col-md-4 col-sm-3 col-xs-12'])}}
+                <div class="col-md-4">
+                    <table class="table table-striped responsive-utilities jambo_table bulk_action">
+                        <thead>
+                            <tr class="headings">
+                                <th class="centered column-title">Hora</th>
+                                <th class="centered column-title"></th>                                        
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @for ($h=8; $h<22; $h++)
+                            <tr class="even pointer">                                                                     
+                                <td class="centered ">{{$h}}:00 hs</td>
+                                <td class="centered "> {{Form::radio('hora_ini',$h , false, array('class' => 'hora_ini'))}} </td>                                                    
+                            </tr>
+                            @endfor
+                        </tbody>
+                    </table>
                 </div>
-
-                <div class="row">
-                    <div class="col-md-8 col-sm-12 col-xs-12">
-                        {{Form::submit('Guardar', ['class'=>'btn btn-success pull-right'])}}
-                        <a class="btn btn-default pull-right" href="{{ route('freeHour.index') }}">Cancelar</a>
-                    </div>
-                </div>
-
-                {{Form::close()}}
-
             </div>
+
+            <div class="form-group">
+                {{Form::label('Curso de PSP*',null,['class'=>'control-label col-md-4 col-sm-3 col-xs-12'])}}
+                <div class="col-md-4">
+                    <select name="Proceso_de_Psp" id="Proceso_de_Psp" class="form-control" required="required">
+                        @if($pspproc!=null)
+                        <option value="">-- Seleccione --</option>
+                        @foreach( $pspproc as $psp)
+                        @if($psp!=null)
+                        <option value="{{$psp->id}}">{{$psp->Course->Nombre}}</option>
+                        @endif
+                        @endforeach
+                        @endif
+                    </select>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-md-8 col-sm-12 col-xs-12">
+                    {{Form::submit('Guardar', ['class'=>'btn btn-success pull-right'])}}
+                    <a class="btn btn-default pull-right" href="{{ route('freeHour.index') }}">Cancelar</a>
+                </div>
+            </div>
+
+            {{Form::close()}}
+
         </div>
     </div>
-    <link rel="stylesheet" href="http://code.jquery.com/ui/1.10.1/themes/base/jquery-ui.css" />
-    <script src="http://code.jquery.com/ui/1.10.1/jquery-ui.js"></script>
-    <script>
-        $(function () {
-            var today = new Date();
+</div>
+<link rel="stylesheet" href="http://code.jquery.com/ui/1.10.1/themes/base/jquery-ui.css" />
+<script src="http://code.jquery.com/ui/1.10.1/jquery-ui.js"></script>
+<script>
+    $(function () {
+        var today = new Date();
 
-            $("#fecha").datepicker({
-                format: "dd-mm-yyyy",                                                                
-                startDate: "today",
-                language: "es",                
-                todayHighlight: true,
-            });
+        $("#fecha").datepicker({
+            format: "dd-mm-yyyy",                                                                
+            startDate: "today",
+            language: "es",                
+            todayHighlight: true,
+        });
 
         
-        });
-    </script>
-    
-    @endsection
+    });
+</script>
+
+@endsection
